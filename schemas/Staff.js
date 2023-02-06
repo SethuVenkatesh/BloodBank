5bc47c40e4d7aeb3cb411736f8f7cd736e1edc39
SethuVenkatesh <113571668+SethuVenkatesh@users.noreply.github.com>
2023-02-06 12:49:59 +0530

    Delete schemas directory

--- schemas/Staff.js
+++
@@ -1,9 +0,0 @@
-const mongoose =require('mongoose');
-
-const Staff=mongoose.Schema({
-    staffname:String,
-    staffmail:String,
-    staffpassword:String,
-    organisation:String
-})
-module.exports=mongoose.model("staffs",Staff);
--- schemas/User.js
+++
@@ -1,8 +0,0 @@
-const mongoose =require('mongoose');
-
-const User=mongoose.Schema({
-    username:String,
-    email:String,
-    password:String
-})
-module.exports=mongoose.model("users",User);
